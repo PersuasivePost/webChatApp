9d483b3f3c917dc9cb710ac240c40b91a8591d50
Ashvatth Joshi <codingash27@gmail.com>
2025-06-05 01:12:37 +0530

    added csrf-protection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { CsrfTokenController } from 'src/csrf-token.controller';
 
 @Module({
   imports: [PrismaModule, UsersModule, AuthModule],
-  controllers: [],
+  controllers: [CsrfTokenController],
   providers: [],
 })
 export class AppModule {}
