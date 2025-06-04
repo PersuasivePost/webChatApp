eb27775b483dabdf7c91980ab273186517d3f14d
Ashvatth Joshi <codingash27@gmail.com>
2025-06-05 00:48:01 +0530

    added cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet()); //
+  app.enableCors(); // Enable CORS for all origins
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
